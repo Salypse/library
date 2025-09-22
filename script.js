f9e2065c1addcb5c870e02f17653a19000fcbe97
Salypse <84153483+Salypse@users.noreply.github.com>
2025-09-22 13:32:18 -0500

    Create new card for every book in library

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const library = document.querySelector(".library");
+
 const myLibrary = [];
 
 function Book(name, author, pages, genre,) {
@@ -12,3 +14,20 @@ function addBookToLibrary (name, author, pages, genre) {
     let newBook = new Book(name, author, pages, genre)
     myLibrary.push(newBook)
 }
+
+function displayLibrary(books) {
+    for (book in books) {
+        let newBookDiv = document.createElement("div");
+        newBookDiv.classList.add("book-card");
+
+        library.appendChild(newBookDiv);
+    }
+}
+
+/* Placeholder books for testing */
+addBookToLibrary("Mistborn", "Brandon Sanderson", "541", "Fantasy")
+addBookToLibrary("Red Rising", "Pierce Brown", "382", "Science Fiction")
+addBookToLibrary("The FellowShip of the Ring", "J. R. R. Tolkien", "423", "Fantasy")
+
+/* Shows all books on user screen */
+displayLibrary(myLibrary)

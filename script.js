2380e022af67ca3e835e18b3264ddf8e6dba5f11
Salypse <84153483+Salypse@users.noreply.github.com>
2025-09-22 11:30:48 -0500

    Create book constructor and add to library func

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World!")
+const myLibrary = [];
+
+function Book(name, author, pages, genre,) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.genre = genre;
+    this.id = crypto.randomUUID()
+}
+
+function addBookToLibrary (name, author, pages, genre) {
+    let newBook = new Book(name, author, pages, genre)
+    myLibrary.push(newBook)
+}

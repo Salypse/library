b853bcb67c3f839f85c3bdab36d78fdb6cd0726f
Salypse <84153483+Salypse@users.noreply.github.com>
2025-09-23 11:01:28 -0500

    Create library book card design

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ const library = document.querySelector(".library");
 
 const myLibrary = [];
 
-function Book(name, author, pages, genre,) {
+function Book(name, author, pages, genre, readStatus) {
     this.name = name;
     this.author = author;
     this.pages = pages;
     this.genre = genre;
+    this.readStatus = readStatus;
     this.id = crypto.randomUUID()
 }
 
-function addBookToLibrary (name, author, pages, genre) {
-    let newBook = new Book(name, author, pages, genre)
+function addBookToLibrary (name, author, pages, genre, readStatus) {
+    let newBook = new Book(name, author, pages, genre, readStatus)
     myLibrary.push(newBook)
 }
 
@@ -22,9 +23,15 @@ function displayLibrary(books) {
 
         let fullHtml = `
         <h1>${book.name}</h1>
-        <p>By: ${book.author}</p>
-        <p>Page Count: ${book.pages}<p>
-        <p>Genre: ${book.genre}<p>
+        <p class="book-author">By: ${book.author}</p>
+        <div class="book-info">
+            <p>Page Count: ${book.pages}</p>
+            <p>Genre: ${book.genre}</p>
+        </div>
+        <div class="book-buttons">
+            <button class="read-status">${book.readStatus}</button> 
+            <button class="remove-book">Remove Book</button>  
+        </div>
         `
         newBookDiv.innerHTML = fullHtml;
         library.appendChild(newBookDiv);
@@ -32,9 +39,9 @@ function displayLibrary(books) {
 }
 
 /* Placeholder books for testing */
-addBookToLibrary("Mistborn", "Brandon Sanderson", "541", "Fantasy")
-addBookToLibrary("Red Rising", "Pierce Brown", "382", "Science Fiction")
-addBookToLibrary("The FellowShip of the Ring", "J. R. R. Tolkien", "423", "Fantasy")
+addBookToLibrary("Mistborn", "Brandon Sanderson", "541", "Fantasy", "Read")
+addBookToLibrary("Red Rising", "Pierce Brown", "382", "Science Fiction", "Read")
+addBookToLibrary("The FellowShip of the Ring", "J. R. R. Tolkien", "423", "Fantasy", "Not Read")
 
 /* Shows all books on user screen */
 displayLibrary(myLibrary)
